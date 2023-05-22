8b712b17ea1fc7a60ae31ebae7dc5c854d22ebd6
humaidan <humaidan@gmail.com>
2023-05-22 11:58:14 +0300

    chapter 2 end v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,38 @@
 module.exports = {
-  "parser": 'babel-eslint',
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  parser: "@babel/eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended"],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
     },
-    "sourceType": "module"
+    sourceType: "module",
   },
-  "plugins": [ "react" ],
-  "rules": {
-    "indent": ["error", 2],
-    "linebreak-style": ["error","unix"],
-    "quotes": ["error","single"],
-    "semi": ["error","always"],
-    "no-console": ["warn", { "allow": ["info", "error"] }]
-  }
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    indent: ["error", 2, {SwitchCase: 1}],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-console": [
+      "warn",
+      { allow: ["clear", "info", "error", "dir", "trace"]}
+    ]
+  },
 };
